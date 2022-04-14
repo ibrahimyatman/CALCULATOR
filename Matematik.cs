ed09cf13e0008d025ccf20f64245d6c4be2eb3f2
yatman.ibrahim.73@gmail.com <yatman.ibrahim.73@gmail.com>
2022-04-14 10:13:11 +0300

    Proje dosyası ekle.

---
+++ Matematik.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calismaa
+{
+    public class math
+    {
+
+        public void DoOperation()
+        {
+            Console.WriteLine("Please select the action you want to take");
+            Console.WriteLine("Press 1 for addition(+) operations");
+            Console.WriteLine("Read 2 for subtract(-) processors");
+            Console.WriteLine("Press the 3 key for multiplication(*) operations");
+            Console.WriteLine("Press 4 for division(/) operations");
+            Console.WriteLine("Press q to exit");
+            
+        }
+        
+        
+        public int MultipOp(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public double DivisionOp(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public int ExtractOp(int num1, int num2) 
+        {
+            return num1 - num2;
+        }
+
+        public int CollecOp(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calismaa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int entNum1 = 0;
+            int entNum2 = 0;
+            int result = 0;
+            bool endApp = true;    
+            math math = new math();
+            Console.WriteLine("Welcome to the c# calculator app");
+            while (endApp)
+            {
+                math.DoOperation();
+                string op =Console.ReadLine();
+                switch (op)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("Type a number, and then press Enter:");
+                            entNum1 =Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Type another number, and then press Enter: ");
+                            entNum2 =Convert.ToInt32(Console.ReadLine());
+                           result= math.CollecOp(entNum1,entNum2);
+                            Console.WriteLine("subtraction result =" + ""+ result);
+                            break;
+                            
+
+                        }
+                        case "2":
+
+                        {
+                            Console.WriteLine("Type a number, and then press Enter:");
+                            entNum1 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Type another number, and then press Enter: ");
+                            entNum2 = Convert.ToInt32(Console.ReadLine());
+                            result=math.ExtractOp(entNum1,entNum2);
+                            Console.WriteLine("result of multiplication =" + "" + result);
+                            break;
+
+
+                        }
+                        case"3":
+                        {
+                            Console.WriteLine("Type a number, and then press Enter:");
+                            entNum1 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Type another number, and then press Enter: ");
+                            entNum2 = Convert.ToInt32(Console.ReadLine());
+                           result=math.MultipOp(entNum1,entNum2);
+                            Console.WriteLine("sum of entered numbers =" + "" + result);
+                            break;
+
+
+                        }
+                        case"4":
+                        {
+
+                            Console.WriteLine("Type a number, and then press Enter:");
+                            entNum1 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Type another number, and then press Enter: ");
+                            entNum2 = Convert.ToInt32(Console.ReadLine());
+                           double result1 = math.DivisionOp(entNum1, entNum2);
+                            Console.WriteLine("result of division =" + "" + result1);
+                            break;
+                        }
+                        case"q":
+                        {
+                            Console.WriteLine("Good day");
+                            endApp = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Please enter a valid character");
+                            break;
+                        }
+
+
+                }
+                Console.WriteLine("************************************");
+            }
+            
+
+        }
+    }
+}
